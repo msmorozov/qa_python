978224d82753b4df6bcb878e3e17869a797a0141
morozovms <morozovms@list.ru>
2024-01-30 17:57:17 +0300

    fixed the comments

--- conftest.py
+++ conftest.py
@@ -1,23 +1,6 @@
 import pytest
 
 
-# Названия книг
-@pytest.fixture
-def first_book():
-    return 'Гордость и предубеждение и зомби'
-
-@pytest.fixture
-def second_book():
-    return 'Что делать, если ваш кот хочет вас убить'
-
-@pytest.fixture
-def third_book():
-    return 'Приключения Горка и Морка'
-
-@pytest.fixture
-def fourth_book():
-    return 'Собачка Мяв'
-
 # Жанры книг
 @pytest.fixture
 def genre_detectives():
--- test_tests.py
+++ test_tests.py
@@ -1,4 +1,5 @@
 from main import BooksCollector
+import data
 import pytest
 
 
@@ -11,13 +12,13 @@ class TestBooksCollector:
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
 
-    def test_add_new_book_add_two_books(self,first_book,second_book):
+    def test_add_new_book_add_two_books(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
         # добавляем две книги
-        collector.add_new_book(first_book)
-        collector.add_new_book(second_book)
+        collector.add_new_book(data.books[0])
+        collector.add_new_book(data.books[1])
 
         # проверяем, что добавилось именно две
         assert len(collector.books_genre) == 2
@@ -30,13 +31,11 @@ class TestBooksCollector:
         [
             ('valid_name', 1),  # Пример валидного имени
             ('1', 1),  # Пример другого валидного имени
-            ('', 0),  # Пустая строка, если это допустимо
             ('123456789012345678901234567890123456789', 1),  # Имя длиной 39 символ
             ('1234567890123456789012345678901234567890', 1),  # Имя длиной 40 символов
-            ('12345678901234567890123456789012345678901', 0),  # Имя длиной 41 символов
         ]
     )
-    def test_add_new_book_add_book_40_letters(self, book_name, expected_books_count):
+    def test_add_new_book_checking_valid_book_name(self, book_name, expected_books_count):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
@@ -46,39 +45,56 @@ class TestBooksCollector:
         #осуществляем проверку
         assert len(collector.get_books_genre()) == expected_books_count
 
-    def test_add_new_book_add_two_identical_book(self, first_book):
+        @pytest.mark.parametrize(
+            'book_name, expected_books_count',
+            [
+                ('', 0),  # Пустая строка, если это допустимо
+                ('12345678901234567890123456789012345678901', 0),  # Имя длиной 41 символов
+            ]
+        )
+        def test_add_new_book_checking_unvalid_book_name(self, book_name, expected_books_count):
+            # создаем экземпляр (объект) класса BooksCollector
+            collector = BooksCollector()
+
+            # добавляем книгу с тестовым именем
+            collector.add_new_book(book_name)
+
+            # осуществляем проверку
+            assert len(collector.get_books_genre()) == expected_books_count
+
+    def test_add_new_book_add_two_identical_book(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
         # добавляем две одинаковых книги
-        collector.add_new_book(first_book)
-        collector.add_new_book(first_book)
+        collector.add_new_book(data.books[0])
+        collector.add_new_book(data.books[0])
 
         # проверяем, что добавилась одна из них
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 1
         assert len(collector.get_books_genre()) == 1
 
-    def test_set_book_genre_for_new_books(self, first_book, genre_detectives):
+    def test_set_book_genre_for_new_books(self, genre_detectives):
         collector = BooksCollector()
 
-        collector.add_new_book(first_book)
-        collector.set_book_genre(first_book, genre_detectives)
+        collector.add_new_book(data.books[0])
+        collector.set_book_genre(data.books[0], genre_detectives)
         actual_genre = collector.get_books_genre()
 
         # проверяем, полученный словарь жанров
-        assert actual_genre == {first_book: genre_detectives}
+        assert actual_genre == {data.books[0]: genre_detectives}
 
-    def test_get_book_genre_correct_genre_for_the_book_is_returned(self, first_book, genre_detectives):
+    def test_get_book_genre_correct_genre_for_the_book_is_returned(self, genre_detectives):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
         # добавляем новую книгу
-        collector.add_new_book(first_book)
+        collector.add_new_book(data.books[0])
         # устанавливаем добавленной книге существующий жанр
-        collector.set_book_genre(first_book,genre_detectives)
+        collector.set_book_genre(data.books[0],genre_detectives)
 
         # проверяем, что в списоке books_genre есть добавленная книга с существующим жанром
-        actual_genre = collector.get_book_genre(first_book)
+        actual_genre = collector.get_book_genre(data.books[0])
 
         # ожидаемое значение
         expected_genre = genre_detectives
@@ -153,92 +169,91 @@ class TestBooksCollector:
 
         assert len(expected_books) == expected_books_count
 
-    def test_get_books_for_children_show_name_books_without_age_rayting(self, first_book, second_book,third_book,
-                                                                     fourth_book,genre_detectives,genre_fantastic,
+    def test_get_books_for_children_show_name_books_without_age_rayting(self, genre_detectives,genre_fantastic,
                                                                      genre_сartoons):
         collector = BooksCollector()
 
-        collector.add_new_book(first_book)
-        collector.set_book_genre(first_book,genre_detectives)
+        collector.add_new_book(data.books[0])
+        collector.set_book_genre(data.books[0],genre_detectives)
 
-        collector.add_new_book(third_book)
-        collector.set_book_genre(third_book, genre_fantastic)
+        collector.add_new_book(data.books[2])
+        collector.set_book_genre(data.books[2], genre_fantastic)
 
-        collector.add_new_book(fourth_book)
-        collector.set_book_genre(fourth_book, genre_сartoons)
+        collector.add_new_book(data.books[3])
+        collector.set_book_genre(data.books[3], genre_сartoons)
 
         expected_books = collector.get_books_for_children()
 
-        assert expected_books == [third_book, fourth_book]
+        assert expected_books == [data.books[2], data.books[3]]
 
-    def test_add_book_in_favorites_and_show_them(self, first_book, third_book):
+    def test_add_book_in_favorites_and_show_them(self):
         collector = BooksCollector()
 
-        collector.add_new_book(first_book)
-        collector.add_new_book(third_book)
+        collector.add_new_book(data.books[0])
+        collector.add_new_book(data.books[2])
 
-        collector.add_book_in_favorites(third_book)
+        collector.add_book_in_favorites(data.books[2])
         favorites_books = collector.get_list_of_favorites_books()
 
-        assert favorites_books == [third_book]
+        assert favorites_books == [data.books[2]]
 
-    def test_add_book_in_favorites_and_show_non_existent_book(self, first_book, third_book):
+    def test_add_book_in_favorites_and_show_non_existent_book(self):
         collector = BooksCollector()
 
-        collector.add_new_book(first_book)
-        collector.add_new_book(third_book)
+        collector.add_new_book(data.books[0])
+        collector.add_new_book(data.books[2])
 
         collector.add_book_in_favorites('Не существующая книга')
         favorites_books = collector.get_list_of_favorites_books()
 
         assert len(favorites_books) == 0
 
-    def test_add_book_in_favorites_twice(self,first_book, third_book):
+    def test_add_book_in_favorites_twice(self):
         collector = BooksCollector()
 
-        collector.add_new_book(first_book)
-        collector.add_new_book(third_book)
+        collector.add_new_book(data.books[0])
+        collector.add_new_book(data.books[2])
 
-        collector.add_book_in_favorites(third_book)
-        collector.add_book_in_favorites(third_book)
+        collector.add_book_in_favorites(data.books[2])
+        collector.add_book_in_favorites(data.books[2])
         favorites_books = collector.get_list_of_favorites_books()
 
         assert len(favorites_books) == 1
 
-    def test_delete_book_from_favorites(self, first_book,third_book):
+    def test_delete_book_from_favorites(self):
         collector = BooksCollector()
 
-        collector.add_new_book(first_book)
-        collector.add_new_book(third_book)
+        collector.add_new_book(data.books[0])
+        collector.add_new_book(data.books[2])
 
-        collector.add_book_in_favorites(first_book)
-        collector.add_book_in_favorites(third_book)
+        collector.add_book_in_favorites(data.books[0])
+        collector.add_book_in_favorites(data.books[2])
 
-        collector.delete_book_from_favorites(third_book)
+        collector.delete_book_from_favorites(data.books[2])
         favorites_books = collector.get_list_of_favorites_books()
 
         assert len(favorites_books) == 1
-        assert favorites_books == [first_book]
+        assert favorites_books == [data.books[0]]
 
-    def test_delete_book_from_favorites_non_existent_book(self, first_book):
+    def test_delete_book_from_favorites_non_existent_book(self):
         collector = BooksCollector()
 
-        collector.add_new_book(first_book)
-        collector.add_book_in_favorites(first_book)
+        collector.add_new_book(data.books[0])
+        collector.add_book_in_favorites(data.books[0])
 
         collector.delete_book_from_favorites('Не существующая книга')
         favorites_books = collector.get_list_of_favorites_books()
 
         assert len(favorites_books) == 1
 
-    def test_get_list_of_favorites_books(self, first_book, second_book):
+    def test_get_list_of_favorites_books(self):
         collector = BooksCollector()
 
-        collector.add_new_book(first_book)
-        collector.add_new_book(second_book)
+        collector.add_new_book(data.books[0])
+        collector.add_new_book(data.books[1])
 
-        collector.add_book_in_favorites(first_book)
-        collector.add_book_in_favorites(second_book)
+        collector.add_book_in_favorites(data.books[0])
+        collector.add_book_in_favorites(data.books[1])
 
         favorites_books = collector.get_list_of_favorites_books()
 
